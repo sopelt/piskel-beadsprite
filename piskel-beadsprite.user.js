11436d3af604073f69fa185d1c48d716cbd3174d
Simon Opelt <simon@opelt.net>
2018-02-12 22:47:44 +0100

    Create piskel-beadsprite.user.js

---
+++ piskel-beadsprite.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Piskel Beadsprite Export
+// @namespace    https://github.com/sopelt
+// @version      1.0.0
+// @description  Add an export option for beadsprite use-cases.
+// @author       Simon Opelt
+// @license      MIT
+// @copyright    2018, Simon Opelt
+// @match        https://www.piskelapp.com/p/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js#sha256=YHd/E8OX5o5GcqyHHas3xH3i7aP8z8KmMrL3WLMJfIY=
+// @grant        none
+// ==/UserScript==
